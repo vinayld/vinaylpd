f51633f960b79dc84c527e0d76d400279fbe56b4
vinayld <vinayld05@gmail.com>
2023-08-30 14:14:12 +0530

    first commit

---
+++ vinayld/src/main/java/vinayld/vinaylpd.java
@@ -0,0 +1,13 @@
+package vinayld;
+
+public class vinaylpd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		String s=("vinay");
+		System.out.println(i);
+		System.out.println(s);
+	}
+
+}
